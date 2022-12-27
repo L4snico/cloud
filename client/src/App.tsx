ebac30b1642e0681948eab0965de191611453bae
Nicolas <nicolasmuller.barros@gmail.com>
2022-12-27 11:14:04 -0300

    creating and using class Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@mui/material"
+import { Component } from "classes"
 
-function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-			<Button variant="contained">MUI</Button>
-		</>
-	)
+export default class App extends Component {
+	static build(): JSX.Element {
+		return (
+			<>
+				<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				<Button variant="contained">MUI</Button>
+			</>
+		)
+	}
+
+	render(): JSX.Element {
+		return <App.build />
+	}
 }
-
-export default App
