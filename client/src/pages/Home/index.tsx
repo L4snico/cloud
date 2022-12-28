6ff6f3ca5b1803e18e2d5e6ee8f2e022b8008e4b
Nicolas <nicolasmuller.barros@gmail.com>
2022-12-27 23:45:41 -0300

    creating and using TopBarHome

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@mui/material"
 import { Component } from "classes"
+import { TopBarHome } from "components"
 
 export class HomePage extends Component {
 	static build(): JSX.Element {
 		return (
 			<>
-				<h1 className="text-3xl font-bold underline">Hello world!</h1>
-				<Button variant="contained">MUI</Button>
+				<TopBarHome />
 			</>
 		)
 	}
